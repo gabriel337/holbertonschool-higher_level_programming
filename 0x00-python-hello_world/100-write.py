f753d1abe92a4288e131433404c582e5e02d96ee
gabriel337 <g.david.lopezruiz@gmail.com>
2021-12-23 03:30:10 +0000

    changed code

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,7 +1,4 @@
 #!/usr/bin/python3
 import sys
-if sys is None:
-    exit(1)
-else:
-    sys.stdout.write("and that piece of \
-art is useful - Dora Korpar, 2015-10-19\n")
+sys.stdout.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+exit(1)
