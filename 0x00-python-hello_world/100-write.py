1064743567be9c6c54966a71f083da27cc5c22ee
gabriel337 <g.david.lopezruiz@gmail.com>
2021-12-23 03:27:53 +0000

    fix task

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,3 +1,7 @@
 #!/usr/bin/python3
 import sys
-sys.stdout.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+if sys is None:
+    exit(1)
+else:
+    sys.stdout.write("and that piece of \
+art is useful - Dora Korpar, 2015-10-19\n")
