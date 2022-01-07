3feed00a64c7d3057e8a60b303fe241da13a9916
gabriel337 <g.david.lopezruiz@gmail.com>
2022-01-07 17:48:38 +0000

    added task

--- 0x03-python-data_structures/7-add_tuple.py
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -5,12 +5,12 @@ def add_tuple(tuple_a=(), tuple_b=()):
     elif len(tuple_a) == 1:
         tuple_a += (0, )
     elif len(tuple_a) > 2:
-        tuple_a += tuple_a[:2]
+        tuple_a = tuple_a[:2]
     if len(tuple_b) == 0:
         tuple_b += (0, 0)
     elif len(tuple_b) == 1:
         tuple_b += (0, )
     elif len(tuple_b) > 2:
-        tuple_b += tuple_b[:2]
+        tuple_b = tuple_b[:2]
     result = (tuple(sum(num) for num in zip(tuple_a, tuple_b)))
     return result
