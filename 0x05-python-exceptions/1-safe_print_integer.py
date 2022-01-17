479bb36997666ec14d0b471cac9fef67f2998796
gabriel337 <g.david.lopezruiz@gmail.com>
2022-01-17 19:40:59 +0000

    change code

--- 0x05-python-exceptions/1-safe_print_integer.py
+++ 0x05-python-exceptions/1-safe_print_integer.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 def safe_print_integer(value):
-    while value != str(value):
         try:
-            print("{}".format(value))
+            print("{:d}".format(value))
             return True
         except ValueError:
             return False
