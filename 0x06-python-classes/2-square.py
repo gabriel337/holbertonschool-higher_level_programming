cf308497d4444743960f2c28246519e4802f3c47
gabriel337 <g.david.lopezruiz@gmail.com>
2022-01-19 15:09:21 +0000

    add task

---
+++ 0x06-python-classes/2-square.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""
+Module 2-square
+Defines class Square with private attribute size
+"""
+
+
+class Square:
+    """
+    class Square definition
+
+    Args:
+        size: size of a side in square
+    """
+    __size = None
+
+    def __init__(self, size=0):
+        """
+        Initializes square
+
+        Attributes:
+            size: size of a side of a square
+        """
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
