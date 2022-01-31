9bbee4bbb82f9c8f310c967bd875b72f31fc6cca
gabriel337 <g.david.lopezruiz@gmail.com>
2022-01-31 18:03:34 +0000

    add task

---
+++ 0x0A-python-inheritance/0-lookup.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+module 0-lookup
+Returns the list of available attributes and methods of an object
+"""
+
+
+def lookup(obj):
+    """ returns attributes and methods"""
+    return(dir(obj))
