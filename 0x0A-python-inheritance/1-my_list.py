211738349af6292ad7430b9546d79895dc59260a
gabriel337 <g.david.lopezruiz@gmail.com>
2022-02-01 00:25:25 +0000

    add python file

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Module 1-my_list
+contains class MyList
+"""
+class MyList(list):
+    """Inherits from list"""
+    def print_sorted(self):
+        """prints sorted list"""
+        print(sorted(self))
