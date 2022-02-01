0dc011cf00121b5ad99278ee00ec99f72c773633
gabriel337 <g.david.lopezruiz@gmail.com>
2022-02-01 00:49:52 +0000

    add task

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+"""
+Module 6-base_geometry
+Contains class BaseGeometry
+
+Method:
+    area(self)
+"""
+
+
+class BaseGeometry:
+    """
+    has a empty method for area that raises an exep
+    """
+    def area(self):
+        """ raises an exeption"""
+        raise Exception("area() is not implemented")
