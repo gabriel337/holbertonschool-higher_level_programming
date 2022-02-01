1982467f30b44a0d10b4f5ba9ff15a5128ea9047
gabriel337 <g.david.lopezruiz@gmail.com>
2022-02-01 00:25:53 +0000

    add task

---
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+Module 2-is_same_class
+Contains method is_same_calss
+"""
+
+
+def is_same_class(obj, a_class):
+    """
+    return true if is instance or false if not
+    """
+    return type(obj) == a_class
