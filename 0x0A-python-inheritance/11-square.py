df10bbbfe564a238d137452a3f38aa681b56fe74
gabriel337 <g.david.lopezruiz@gmail.com>
2022-02-02 20:53:50 +0000

    add task

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python3
+"""
+Module 11-rectangle
+Contains parent class BaseGeometry
+with public instance method area and integer_validator
+Contains subclass Rectangle
+with instantiation of private attributes width and height, validated by parent
+contains subclass Square
+with instantiation of private attribute size.
+"""
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """
+    Methods:
+        __init__(self, size)
+    """
+    def __init__(self, size):
+        """ Validates and initialize width and height"""
+        self.__size = size
+        super().integer_validator("size", size)
+        super().integer_validator("size", size)
+        super().__init__(size, size)
+
+    def __str__(self):
+        return ("[Square] {}/{}".format(self.__size, self.__size))
