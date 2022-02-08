9a9929449f1c4723af9a6a1b7a2ffc9e55d27f61
gabriel337 <g.david.lopezruiz@gmail.com>
2022-02-08 15:39:03 +0000

    add task

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -38,11 +38,11 @@ class Square(Rectangle):
                 .format(self.id, self.x, self.y, self.size, self.size))
 
     def update(self, *args, **kwargs):
-         """ adds input to arguments """
-         ls = ['id', 'size', 'x', 'y']
-         if args:
-             for idx in range(len(args)):
+        """ adds input to arguments """
+        ls = ['id', 'size', 'x', 'y']
+        if args:
+            for idx in range(len(args)):
                 setattr(self, ls[idx], args[idx])
-         else:
-             for key, value in kwargs.items():
-                 setattr(self, key, value)
+        else:
+            for key, value in kwargs.items():
+                setattr(self, key, value)
