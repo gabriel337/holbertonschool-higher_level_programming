4a339b4c318c718433080fecd91818097d5af0a4
gabriel337 <g.david.lopezruiz@gmail.com>
2022-02-09 01:45:13 +0000

    add task

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -46,3 +46,7 @@ class Square(Rectangle):
         else:
             for key, value in kwargs.items():
                 setattr(self, key, value)
+
+    def to_dictionary(self):
+        """ returns Square instance to dictionary representation """
+        return {'id': self.id, 'x': self.x, 'size': self.size, 'y': self.y}
