0fc8339d5e7010fc1de2634576368cd285568649
gabriel337 <g.david.lopezruiz@gmail.com>
2022-03-09 03:32:40 +0000

    add task 15

---
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -0,0 +1,7 @@
+-- lists the number of records with the same score in the table second_table
+-- of the db htbn_0c_0
+
+SELECT score, count(*) AS number
+FROM second_table
+GROUP BY score
+ORDER BY score DESC;
