4f48b86d08d44ae876a4651741d9bcd9067b59ac
gabriel337 <g.david.lopezruiz@gmail.com>
2022-04-05 02:15:05 +0000

    add task 8

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,14 @@
+#!/usr/bin/node
+const num = process.argv[2];
+let x = 0;
+let square = '';
+if (isNaN(num)) {
+  console.log('Missing size');
+} else {
+  for (x = 0; x < num; x++) {
+    square = square.concat('X');
+  }
+  for (x = 0; x < num; x++) {
+    console.log(square);
+  }
+}
