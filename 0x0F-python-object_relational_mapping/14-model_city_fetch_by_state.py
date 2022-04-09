624ec95a9ec873815134f6c57e8656f08ddf45d3
gabriel337 <g.david.lopezruiz@gmail.com>
2022-04-09 18:10:29 +0000

    fix syntax details

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -5,18 +5,17 @@
 from sys import argv
 from model_state import Base, State
 from model_city import City
-from sqlalchemy.orm import sessionmaker
-from sqlalchemy import (create_engine)
+from sqlalchemy import create_engine, orm
 
 if __name__ == "__main__":
     user = argv[1]
     passwd = argv[2]
     db = argv[3]
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.
-                           format(user, passwd, db), pool_pre_ping=True)
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(*argv[1:4]), pool_pre_ping=True)
 
     Base.metadata.create_all(engine)
-    Session = sessionmaker(bind=engine)
+    Session = orm.sessionmaker(bind=engine)()
     cities = (session
               .query(State.name, City.id, City.name)
               .filter(City.state_id == State.id)
