d7735b4251f111cf2190bb51c3ecdfbeb81b5087
gabriel337 <g.david.lopezruiz@gmail.com>
2022-04-09 18:21:31 +0000

    change syntax

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,20 +1,22 @@
 #!/usr/bin/python3
-"""
-model_city.py that contains the class
+'''
+model_city.py contains the class
 definition of a City
-"""
+'''
 
-from model_state import State
+
 from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
+from model_state import State
 
 Base = declarative_base()
 
 
 class City(Base):
-    """
+    '''
     Inherits from Base and links MySQL 'state'
-    """
+    '''
+
     __tablename__ = 'cities'
     id = Column(Integer, unique=True, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
