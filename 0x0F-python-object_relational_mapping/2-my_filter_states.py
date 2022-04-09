f2073bb76c839cf46465f4b7fa0ee1bea4d2c2ac
gabriel337 <g.david.lopezruiz@gmail.com>
2022-04-09 17:54:50 +0000

    change syntax

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,21 +8,21 @@ import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
+    user = argv[1]
+    password = argv[2]
+    database = argv[3]
+    state_search = argv[4]
 
-    # connect to database
-    db = MySQLdb.connect(host="localhost",
+    connect = MySQLdb.connect(host="localhost",
                          port=3306,
-                         user=argv[1],
-                         passwd=argv[2],
-                         db=argv[3],
-                         state_search=argv[4])
+                         user=user,
+                         passwd=password,
+                         db=database)
 
-    # create cursor to exec queries using SQL; match arg given
-    cursor = db.cursor()
+    cursor = connect.cursor()
     cursor.execute("""SELECT * FROM states WHERE BINARY name='{}'
                     ORDER BY id""".format(state_search))
     for row in cursor.fetchall():
-        if row[1] == argv[4]:
-            print(row)
+        print(row)
     cursor.close()
-    db.close()
+    connect.close()
