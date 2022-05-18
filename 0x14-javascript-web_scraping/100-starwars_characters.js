ca2513fdb1cfb2b2c56ff14e14e10804d6e317a6
gabriel337 <g.david.lopezruiz@gmail.com>
2022-05-18 03:55:28 +0000

    add task 100

---
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+// script that prints all characters of a Star Wars movie
+
+const id = process.argv[2];
+const url = 'http://swapi.co/api/films/' + id;
+const request = require('request');
+
+request(url, function (err, response, body) {
+  if (err) {
+    console.log(err);
+  } else if (response.statusCode === 200) {
+    body = JSON.parse(body);
+    for (const i in body.characters) {
+      request(body.characters[i], function (err, response, body) {
+        if (err) {
+          console.log(err);
+        } else if (response.statusCode === 200) {
+          console.log(JSON.parse(body).name);
+        }
+      });
+    }
+  } else {
+    console.log('Erorr Code:' + response.statusCode);
+  }
+});
