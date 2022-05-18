a78817eef5ed188ffac9e98c51b0ef75361d5d02
gabriel337 <g.david.lopezruiz@gmail.com>
2022-05-18 03:52:48 +0000

    add task 6

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -4,23 +4,20 @@
 const url = process.argv[2];
 const request = require('request');
 
-request(url, function (err, response, body) {
+request(url, (err, response, body) => {
   if (err) {
-    console.log(err);
-  } else if (response.statusCode === 200) {
-    const dic = {};
-    const tasks = JSON.parse(body);
-    for (const i in tasks) {
-      if (tasks[i].completed) {
-        if (dic[tasks[i].userId] === undefined) {
-          dic[tasks[i].usedId] = 1;
-        } else {
-          dic[tasks[i].userId]++;
-        }
-      }
-    }
-    console.log(dic);
-  } else {
-    console.log('Error Code: ' + response.statusCode);
+    console.error(err);
+    return;
   }
+
+  const tasks = JSON.parse(body);
+  const dic = {};
+
+  for (const task of tasks) {
+    if (task.completed) {
+      const userID = task.userId;
+      dic[userID] = (dic[userID] + 1 || 1);
+    }
+  }
+  console.log(dic);
 });
