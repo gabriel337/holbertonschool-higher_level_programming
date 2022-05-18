b1497fd135b876cd89854fba9c818f33ec0ab454
gabriel337 <g.david.lopezruiz@gmail.com>
2022-05-18 03:22:34 +0000

    change syntax

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -2,17 +2,12 @@
 //  script that prints the title of a Star Wars movie
 //  where the episode number matches a given integer
 
-const id = process.argv[2];
-const url = 'http://swapi.co/api/films/' + id;
 const request = require('request');
-
-request(url, function (err, response, body) {
-  if (err) {
-    console.log(err);
-  } else if (response.statusCode === 200) {
-    body = JSON.parse(body);
-    console.log(body.title);
+const url = 'https://swapi-api.hbtn.io/api/films/';
+request(url + process.argv[2], function (error, response, body) {
+  if (error) {
+    console.log(error);
   } else {
-    console.log('Error Code:' + response.statusCode);
+    console.log(JSON.parse(body).title);
   }
 });
