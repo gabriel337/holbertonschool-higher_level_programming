408cc8ecf37e9b2afb63109425d270541c29097f
gabriel337 <g.david.lopezruiz@gmail.com>
2022-05-18 02:58:55 +0000

    fix semistandard error

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // Script that reads and prints the content of a file
 
-let filename = process.argv[2];
+const filename = process.argv[2];
 const fs = require('fs');
 fs.readFile(filename, 'utf8', function (err, data) {
   if (err) {
